3f56267e66d481c4e44386dda7211ee74966303d
www.ranga.com <ranga2007.03.09@gmail.com>
2026-03-09 19:37:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
 
     // Background & Colors (You can use an Image URL or an MP4 Video URL)
     // Example Video: "https://cdn.pixabay.com/video/2020/05/25/40118-422830846_large.mp4"
-    backgroundMedia: "https://videotourl.com/videos/1772957008874-8dd7ead6-ba56-45d0-a0bc-6b67da10ceab.mp4",
+    backgroundMedia: "https://videotourl.com/videos/1773065039030-fe540c2d-9924-4aa1-ae4b-2def1a5b3e74.mp4",
     primaryColor: "#ff0000",
 
     // Audio Link (Note: Direct MP3 link is required here. YouTube links don't play in standard HTML audio players. I've set the album art!)
